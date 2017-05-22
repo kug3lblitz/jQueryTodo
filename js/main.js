60112c3f7d54152eed9138c32a16435c62a3d2e1
Patrick Werner <pwerner2@gmail.com>
2017-05-22 01:14:35 -0400

    works. no storage

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 //even though all li's are selected, 'this' only applies style to the clicked element
 //when comparing colors, colors must be rgb format
 
-$('li').click(function() {
-  if($(this).css('color') === "rgb(128, 128, 128)"){
-      //testing
-      //alert("it's gray");
-      $(this).css({
-        color: 'black',
-        textDecoration: 'initial'
-      })
-  }
-  else {
-    $(this).css({
-      color: 'gray',
-      textDecoration: 'line-through'
-    });
+$('ul').on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+
+//click span to remove todo
+
+$('ul').on("click", "span", function() {
+  //alert('span');
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+  if (event.which === 13){
+    var todoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span><i class=\"fa fa-trash-o\" aria-hidden=\"true\"></i></span> " + todoText + "</li>");
   }
 });
